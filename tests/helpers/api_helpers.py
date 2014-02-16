92e008e09a7478f943f1f2ba3678302cd9247555
George Song <george@55minutes.com>
2014-02-16 13:25:47 -0800

    Rename tests to be more meaningful

--- tests/helpers/api_helpers.py
+++ tests/helpers/api_helpers.py
@@ -1,11 +1,24 @@
 from epo_ops.models import Docdb
 
+data = ('publication', Docdb('1000000', 'EP', 'A1'))
+
+
+def assert_request_success(response):
+    assert response.status_code == 200
+    assert response.headers['X-API'] == 'ops-v3.1'
+
 
 def issue_published_data_request(client):
-    return client.published_data('publication', Docdb('1000000', 'EP', 'A1'))
+    return client.published_data(*data)
 
 
 def assert_published_data_success(client):
-    r = issue_published_data_request(client)
-    assert r.status_code == 200
-    assert r.headers['X-API'] == 'ops-v3.1'
+    assert_request_success(issue_published_data_request(client))
+
+
+def issue_family_request(client):
+    return client.family(*data)
+
+
+def assert_family_success(client):
+    assert_request_success(issue_family_request(client))
--- tests/test_api.py
+++ tests/test_api.py
@@ -8,7 +8,8 @@ from epo_ops import Client, RegisteredClient
 
 from secrets import KEY, SECRET
 from helpers.api_helpers import (
-    assert_published_data_success, issue_published_data_request
+    assert_published_data_success, issue_published_data_request,
+    assert_family_success, issue_family_request,
 )
 
 
@@ -19,33 +20,33 @@ def registered_client(module_storage):
     )
 
 
-def test_real_happy_anonymous(storage):
+def test_anonymous_published_data(storage):
     assert_published_data_success(Client(throttle_history_storage=storage))
 
 
-def test_real_get_access_token(registered_client):
+def test_get_access_token(registered_client):
     assert 'access_token' in registered_client.access_token._content
 
 
-def test_real_happy_registered(registered_client):
+def test_registered_published_data(registered_client):
     assert_published_data_success(registered_client)
 
 
-def test_real_400_invalid_token(registered_client):
+def test_400_invalid_token(registered_client):
     # Put in a token that's invalid, the server will raise 400
     registered_client.access_token.token = 'x34NdKmpABZ8ukqi4juRNQCrv5C5'
     with raises(HTTPError):
         issue_published_data_request(registered_client)
 
 
-def test_real_400_expired_token(registered_client):
+def test_400_expired_token(registered_client):
     # Put in a token that's expired, the server will raise 400 but we should
     # handle it gracefully
     registered_client.access_token.token = 'm34NdKmpABZ8ukqi4juRNQCrv5C5'
     assert_published_data_success(registered_client)
 
 
-def test_real_self_check_expired_token(registered_client):
+def test_self_check_expired_token(registered_client):
     old_token = registered_client.access_token.token
     registered_client.access_token.expiration = datetime.now()
     assert old_token != registered_client.access_token.token
