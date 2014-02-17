5fdb02ea4ae3beab98b9586e68c70f163f894038
George Song <george@55minutes.com>
2014-02-17 11:27:36 -0800

    More setup tweaks

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 import os
-import shlex
 import sys
 
 from __version__ import __version__
@@ -27,19 +25,6 @@ with open('LICENSE') as f:
     license = f.read()
 
 
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = shlex.split(
-            '--cov-report html --cov-report term --cov epo_ops tests'
-        )
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errcode = pytest.main(self.test_args)
-        sys.exit(errcode)
-
 setup(
     name='python-epo-ops-client',
     version=__version__,
@@ -56,20 +41,20 @@ setup(
         'v{}.tar.gz'.format(__version__)
     ),
     packages=packages,
-    package_data={'': ['LICENSE']},
     package_dir={'epo_ops': 'epo_ops'},
     include_package_data=True,
     install_requires=requires,
     license=license,
+    tests_require=['pytest'],
     zip_safe=False,
-    classifiers=(
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-    ),
-    cmdclass={'test': PyTest},
-    tests_require=['pytest'],
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
