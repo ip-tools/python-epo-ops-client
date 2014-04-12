243e645c95db92873e549c155e305fbd07dee98b
George Song <george@55minutes.com>
2014-04-11 19:22:18 -0700

    Work in PyPy!

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
