7d1a8a4baeefea1fc842511572bdbf3fbb4bd393
George Song <george@55minutes.com>
2015-09-20 15:26:01 -0700

    Use dotenv to load our secrets
    Closes #5.

--- tests/secrets.py
+++ tests/secrets.py
@@ -1,4 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals
+)
+
+from os.path import abspath, dirname, join
 import os
+
+from dotenv import load_dotenv
+
+dotenv_path = abspath(join(dirname(__file__), '../.env'))
+load_dotenv(dotenv_path)
 
 APIARY_URL = os.environ['APIARY_URL']
 KEY = os.environ['OPS_KEY']
