c189309c645debacf9d081bd6f1bcea2a0f2b8cd
George Song <george@55minutes.com>
2015-09-20 18:15:38 -0700

    Workaround for tox variable setting

--- tests/secrets.py
+++ tests/secrets.py
@@ -8,6 +8,11 @@ import os
 
 from dotenv import load_dotenv
 
+# Workaround tox environment issue, where these keys would be set to empty
+for k in ('APIARY_URL', 'OPS_KEY', 'OPS_SECRET'):
+    if k in os.environ and not os.environ[k]:
+        del os.environ[k]
+
 dotenv_path = abspath(join(dirname(__file__), '../.env'))
 load_dotenv(dotenv_path)
 
